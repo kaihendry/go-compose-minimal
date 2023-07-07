ef0ce7fdffb738bde7889e1867e491b69f47a717
Kai Hendry <hendry@iki.fi>
2023-07-07 22:05:29 +0100

    saner

--- sane/main.go
+++ sane/main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/apex/gateway/v2"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	slogecho "github.com/samber/slog-echo"
@@ -15,27 +14,19 @@ import (
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	if _, ok := os.LookupEnv("AWS_LAMBDA_FUNCTION_NAME"); ok {
-		logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	}
-
-	// Echo instance
 	e := echo.New()
 
-	// Middleware
 	e.Use(slogecho.New(logger))
 	e.Use(middleware.Recover())
 
-	// Routes
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Welcome to sanit!")
+		return c.String(http.StatusOK, "Welcome to sanity!")
 	})
 
-	if _, ok := os.LookupEnv("AWS_LAMBDA_FUNCTION_NAME"); ok {
-		gateway.ListenAndServe("", e)
-	} else {
-		slog.Info("local development", "port", os.Getenv("PORT"))
-		e.Start(fmt.Sprintf(":%s", os.Getenv("PORT")))
+	err := e.Start(fmt.Sprintf(":%s", os.Getenv("PORT")))
+
+	if err != nil {
+		logger.Error("failed to listen and serve", err)
 	}
 
 }
